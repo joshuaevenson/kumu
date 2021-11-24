9fd811e464cb5a784ff6c0be842094913e515fab
mohsen-velentir <mohsen@velentir.com>
2021-11-24 11:05:08 -1000

    Initial Commit

---
+++ kumu/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  kumu
+//
+//  Created by Mohsen Agsen on 11/24/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
